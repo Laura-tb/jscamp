25ea71fdd2d075442522ac55686d6ac4507af08b
Laura-tb <202150686+Laura-tb@users.noreply.github.com>
2025-10-26 20:20:48 +0100

    Se intenta añadir funcion para paginacion, pero no se termina

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -8,7 +8,7 @@
 
 
 import data from './data.json'
-console.log(data)
+
 import JobCard from './components/JobCard.jsx'
 
 const RESULTS_PER_PAGE = 3
@@ -38,6 +38,33 @@ function Footer() {
     <footer>
       <small>&copy; 2025 DevJobs. Todos los derechos reservados.</small>
     </footer>
+  )
+}
+
+function Pagination() {
+
+  const totalData = data.length
+  const numPag = Math.ceil(totalData / RESULTS_PER_PAGE)
+
+  return (
+    <nav className="pagination">
+      <a href="#"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+        strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M15 6l-6 6l6 6" />
+      </svg></a>
+      <a className="is-active" href="#">1</a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">5</a>
+      <a href="#"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5"
+        strokeLinecap="round" strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M9 6l6 6l-6 6" />
+      </svg></a>
+    </nav>
   )
 }
 
@@ -122,24 +149,8 @@ function App() {
             ))}
           </div>
 
-          <nav className="pagination">
-            <a href="#"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
-              strokeLinecap="round" strokeLinejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M15 6l-6 6l6 6" />
-            </svg></a>
-            <a className="is-active" href="#">1</a>
-            <a href="#">2</a>
-            <a href="#">3</a>
-            <a href="#">4</a>
-            <a href="#">5</a>
-            <a href="#"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5"
-              strokeLinecap="round" strokeLinejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M9 6l6 6l-6 6" />
-            </svg></a>
-          </nav>
+          <Pagination />
+
         </section>
       </main>
 
